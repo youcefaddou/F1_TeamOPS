096ea5c91d2837c62b0a0c3feb8a215314561027
youcefaddou <youcefaddou@gmail.com>
2025-06-23 10:47:20 +0200

    ok

--- src/services/authguard.js
+++ src/services/authguard.js
@@ -6,18 +6,18 @@ const authguard = async (req, res, next) => {
         if (req.session.team) {
             const team = await prisma.team.findUnique({
                 where: {
-                    email: req.session.team.email // Ensure email matches session team
+                    email: req.session.team.email // ensure email matches session team
                 }
             });
             if (team) {
-                req.team = team; // Attach team to request for further use
+                req.team = team; // attach team to request for further use
                 return next();
             }
         }
         throw new Error('Directeur non connecté');
     } catch (error) {
-        console.error('Authguard error:', error); // Debug log
-        res.redirect('/login'); // Redirect to login if not authenticated
+        console.error('Authguard error:', error); 
+        res.redirect('/login'); // redirect to login if not authenticated
     }
 };
 module.exports = authguard
